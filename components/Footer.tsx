9ce55510a79fb5fd874d8bbace7447e983f7962d
teetusp <s6452410018@live.sau.ac.th>
2025-11-04 22:20:41 +0700

    เสร็จ navbar และ footer

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-indigo-700 py-8">
+      <div>
+        <h3 className="text-md text-gray-600 text-center text-white">
+          Created by Developer Team
+          <br />
+          Copyright © 2025 All rights reserved.
+        </h3>
+      </div>
+    </footer>
+  );
+}
---
+++ components/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import rocket from '../assets/rocket.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { CiLogin } from "react-icons/ci";
+
+export default function NavBar() {
+  return (
+    <header className="sticky top-0 z-50 bg-white/90 backdrop-blur-sm shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-20">
+          
+          {/* โลโก้/ชื่อแอป */}
+          <div className="flex items-center space-x-2">
+            <Image src={rocket} alt="Logo" className="w-10 h-10 mr-2" />
+            <h1 className="text-2xl font-black text-indigo-600 tracking-wide">
+              <span className="text-yellow-500">Card</span> <span className="text-red-500">Vocab</span>
+            </h1>
+          </div>
+          <nav className="hidden md:flex space-x-8 items-center">
+            <Link href="#features" className="text-lg font-medium text-gray-600 hover:text-indigo-500 transition duration-150">คุณสมบัติ</Link>
+            <Link href="#about" className="text-lg font-medium text-gray-600 hover:text-indigo-500 transition duration-150">เกี่ยวกับ</Link>
+            <button className="flex items-center px-6 py-2 border border-transparent text-base font-bold rounded-full shadow-lg text-white bg-green-500 hover:bg-green-600 transition duration-200 transform hover:scale-105 cursor-pointer">
+              <CiLogin className="w-5 h-5 mr-2 text-white" />
+              เข้าสู่ระบบ
+            </button>   
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
